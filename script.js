4abcf4e1a986f94a664fc47f77aa738d5e6429b3
lasha kachkachishvili <lasha.katchkatchishvili.1@btu.edu.ge>
2023-11-11 13:19:50 +0400

    seminar

---
+++ script.js
@@ -0,0 +1,46 @@
+// console.log("hello world");
+
+// let parag=document.querySelector(".bitcamp");
+// console.log(parag);
+
+// function changeElementText(){
+//     parag.textContent="hello dom"
+// };
+
+// let button=document.querySelector(".btn");
+
+// function changeColor(){
+//     button.style.color="white";
+//     button.style.background="red"
+// }
+// button.addEventListener("click",()=>{
+//     button.style.color="white";
+//     button.style.background="red"
+// })
+// let container=document.querySelector(".container");
+// function createElement(){
+//     let pTag=document.createElement("p");
+//     pTag.textContent="new paragraph";
+//     pTag.className="new";
+  
+//    container.append(pTag);
+
+// }
+// createElement();
+// createElement();
+// let elements=document.querySelectorAll(".new");
+// elements[0].textContent="first"
+
+
+// let arr=Array.from(container.children);
+// let result=arr.filter((item)=>item.textContent=="bitcamp2");
+// console.log(result)
+// let button=document.querySelector(".btn");
+
+// button.addEventListener("click",()=>{
+//     alert("Button clicked!")
+// })
+
+
+
+
